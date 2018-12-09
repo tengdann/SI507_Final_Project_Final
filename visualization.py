038cda18eebd46a05f702b21c2356aee86acd382
tengdann <tengdann@umich.edu>
2018-12-08 19:08:39 -0500

    Temp

---
+++ visualization.py
@@ -0,0 +1,9 @@
+import sqlite3 as sqlite
+from datetime import datetime
+
+DBNAME = 'final_project.sqlite3'
+
+# Do Flask, and possible plotly connected with flask
+# For Flask structure articles w/ information. Allow option to sort by stuff.
+# Maybe I can do SQL call for some? Or else sorting might be difficult.
+
