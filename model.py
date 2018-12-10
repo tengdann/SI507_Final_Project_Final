35c6c5a2edef7253eb85d44b43b0e8d04cac549d
Danny Teng <tengdann@umich.edu>
2018-12-10 17:47:19 -0500

    Finished css issues

--- model.py
+++ model.py
@@ -70,7 +70,7 @@ def graph_pie(items):
     values = [item[1] for item in items]
     trace = go.Pie(labels = labels, values = values)
     
-    layout = go.Layout(width = 600, height = 500)
+    layout = go.Layout(width = 600, height = 500, autosize = True)
     object = go.Figure(data = [trace], layout = layout)
     
     graphJSON = json.dumps(object, cls = plotly.utils.PlotlyJSONEncoder)
